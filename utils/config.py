6fd374f1a4e2b8eac9a406a7d3fae2156821dc48
psghari <psghari@users.noreply.github.com>
2025-05-17 07:06:01 +0530

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_USER_ID = os.getenv("TELEGRAM_USER_ID")
